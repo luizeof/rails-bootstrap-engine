3e1c56db4387887ba92bfc6ffeb1550648d5d184
Luiz Eduardo Oliveira Fonseca <luizeof@gmail.com>
2013-01-29 21:01:57 -0200

    Inclusão de Overrides para o Twitter Bootstraps Inclusão de Background para o Layout Devise Inclusão dos Métodos do Layout Melhorias no Layout Devise

--- app/helpers/bootstrap_engine_helper.rb
+++ app/helpers/bootstrap_engine_helper.rb
@@ -1,12 +1,41 @@
 # -*- encoding : utf-8 -*-
 module BootstrapEngineHelper
 
+
+
+	# Describes the Vendor Brand Name
+	def vendor_brand
+		"Minha Empresa"
+	end
+	
+	
+	
+	# Describes the Vendor Url
+	def vendor_url
+		'http://www.minhaempresa.com.br'
+	end	
+	
+
+
+	# Describes the Application Brand Name
 	def application_brand
 		"Bootstrap Engine Helper"
 	end
 	
+	
+	
+	# Represents the Page title
 	def page_title(title="")
 		content_for :header_title do "#{title} | #{ application_brand }" end
 	end
+	
+	
+	
+	# Represents the Copyright Info
+	def copyright_info
+		content_tag :p, link_to(raw("&copy; #{vendor_brand} #{Date.today.year}"), vendor_url)
+	end
+	
+	
 
 end
